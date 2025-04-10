0d8749b769838a44e2d972c1c22737ef70f497cc
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-04-10 11:16:38 -0300

    feat: replace income display with FlatList and IncomeCard component for improved rendering

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
-import { Button, Text, View } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { Container } from '../components/Container';
 import { Typography } from '../components/Typography';
 import { useDatabase } from '../hooks/useDatabase';
 import { useEffect, useState } from 'react';
 import { Income } from '../database/types';
 import { getIncomes } from '../database/incomes/getIncomes';
+import { theme } from '../styles/theme';
+
+import { IncomeCard } from '../components/IncomeCard';
 
 export default function Index() {
   const [data, setData] = useState<Income[]>([]);
@@ -20,21 +23,68 @@ export default function Index() {
       });
   }, []);
 
+  // createIncome(database, {
+  //   name: 'Salário',
+  //   amount: 5000,
+  //   type: 1,
+  //   date: new Date().toDateString(),
+  //   installment: null,
+  //   installmentQtd: null,
+  // })
+  //   .then(() => console.log('created 1'))
+  //   .catch((error) => console.log('1', error));
+
+  // createIncome(database, {
+  //   name: 'Pix',
+  //   amount: 2000,
+  //   type: 2,
+  //   date: new Date().toDateString(),
+  //   installment: null,
+  //   installmentQtd: null,
+  // })
+  //   .then(() => console.log('created 2'))
+  //   .catch((error) => console.log('2', error));
+
+  // createIncome(database, {
+  //   name: 'Boleto',
+  //   amount: 1000,
+  //   type: 2,
+  //   date: new Date().toDateString(),
+  //   installment: 3,
+  //   installmentQtd: 5,
+  // })
+  //   .then(() => console.log('created 3'))
+  //   .catch((error) => console.log('3', error));
+
   return (
     <Container>
-      <Typography variant="title">Extrato</Typography>
-      {data.map((item) => {
-        return (
-          <View key={item.id}>
-            <Typography>{item.name}</Typography>
-            <Typography>{item.amount}</Typography>
-            <Typography>{item.type === 1 ? 'Entrada' : 'Saída'}</Typography>
-            {item.installment && <Typography>{item.installment}</Typography>}
-            <Typography>{item.date}</Typography>
-            <Typography>--------------</Typography>
-          </View>
-        );
-      })}
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={(list) => <IncomeCard income={list.item} />}
+        ListHeaderComponent={<Typography variant="title">Extrato</Typography>}
+        ListHeaderComponentStyle={styles.header}
+        stickyHeaderIndices={[0]}
+        style={styles.list}
+        contentContainerStyle={styles.contentContainer}
+        getItemLayout={(data, index) => ({
+          length: theme.sizes.card,
+          offset: theme.sizes.card * index,
+          index,
+        })}
+      />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: theme.colors.background,
+  },
+  list: {
+    height: '100%',
+  },
+  contentContainer: {
+    gap: theme.spacing.lg,
+  },
+});
