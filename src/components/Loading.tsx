35ed367572233dbe4b630cf197daea599d983cea
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-04-11 18:52:18 -0300

    feat: add Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,16 @@
+import LottieView from 'lottie-react-native';
+import { theme } from '../styles/theme';
+
+interface LoadingProps {
+  size?: keyof typeof theme.sizes.loading;
+}
+
+export function Loading({ size = 'lg' }: LoadingProps) {
+  return (
+    <LottieView
+      autoPlay
+      style={theme.sizes.loading[size]}
+      source={require('../../assets/animations/loading.json')}
+    />
+  );
+}
