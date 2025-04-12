0e30d9393851d77af7d6710d15aebdbc62c94d62
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-04-12 11:25:26 -0300

    wip: transaction page

---
+++ src/app/transactions/create.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@/src/components/Container';
+import { Typography } from '@/src/components/Typography';
+
+export default function CreateTransaction() {
+  return (
+    <Container>
+      <Typography>Criar</Typography>
+    </Container>
+  );
+}
