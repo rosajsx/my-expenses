49e5829d0fcfec8f15732e15b0ac9c89c472a310
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-04-14 18:07:58 -0300

    WIP: create transactions

--- src/app/transactions/create.tsx
+++ src/app/transactions/create.tsx
@@ -1,10 +1,47 @@
+import { Button } from '@/src/components/Button';
 import { Container } from '@/src/components/Container';
-import { Typography } from '@/src/components/Typography';
+import { Input } from '@/src/components/Input';
+import { theme } from '@/src/styles/theme';
+import { router } from 'expo-router';
+import { X } from 'lucide-react-native';
+import { KeyboardAvoidingView, StyleSheet, TextInput, View } from 'react-native';
 
 export default function CreateTransaction() {
   return (
     <Container>
-      <Typography>Criar</Typography>
+      <KeyboardAvoidingView style={styles.avoidView}>
+        <View style={styles.modalHeader}>
+          <Button variant="ghost" Icon={X} onPress={router.back} />
+        </View>
+        <View style={styles.content}>
+          <Input label="Nome" />
+          <Input error label="Nome" errorText="Deu erro tio" />
+          <Input editable={false} />
+
+          {/* Data = DatePicker */}
+          {/* Tipo = Switch */}
+        </View>
+        <View style={styles.footer}></View>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  modalHeader: {
+    alignItems: 'flex-end',
+  },
+  avoidView: {
+    flex: 1,
+  },
+  content: {
+    borderWidth: 1,
+    flex: 1,
+    gap: theme.spacing.lg,
+  },
+  footer: {
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  input: {},
+});
