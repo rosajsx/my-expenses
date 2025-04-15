6a6bd1306ed5a3a47027cd327cdffb4772880b43
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-04-15 16:08:22 -0300

    feat: Add create transaction page

--- src/app/transactions/create.tsx
+++ src/app/transactions/create.tsx
@@ -1,41 +1,185 @@
 import { Button } from '@/src/components/Button';
 import { Container } from '@/src/components/Container';
 import { Input } from '@/src/components/Input';
+import { TransactionTypeSwitch } from '@/src/components/TransactionTypeSwitch';
+import { Typography } from '@/src/components/Typography';
 import { theme } from '@/src/styles/theme';
+import { formatCurrency, parseCurrencyToCents } from '@/src/utils';
 import { router } from 'expo-router';
-import { X } from 'lucide-react-native';
-import { KeyboardAvoidingView, StyleSheet, TextInput, View } from 'react-native';
+import { DollarSign, X } from 'lucide-react-native';
+import { useRef, useState } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, TextInput, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useScreenState } from '@/src/hooks/useScreenState';
+import { useDatabase } from '@/src/hooks/useDatabase';
+import { createTransaction } from '@/src/database/transactions/createTransaction';
+import LottieView from 'lottie-react-native';
+import { Loading } from '@/src/components/Loading';
+import Animated, { FadeIn } from 'react-native-reanimated';
+
+const AnimatedTypography = Animated.createAnimatedComponent(Typography);
 
 export default function CreateTransaction() {
+  const [transactionName, setTransactionName] = useState('');
+  const [transactionType, setTransactionType] = useState<number>();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [amount, setAmount] = useState(0);
+
+  const {
+    screenState,
+    handleChangeScreenStateToError,
+    handleChangeScreenStateToLoading,
+    handleChangeScreenStateToSuccess,
+    handleChangeScreenStateToDefault,
+    isScreenStateDefault,
+    isScreenStateError,
+    isScreenStateLoading,
+    isScreenStateSuccess,
+  } = useScreenState();
+  const { database } = useDatabase();
+
+  const currencyValueRef = useRef<TextInput>();
+
+  const isCreateButtonDisabled = !transactionType && !transactionName;
+
+  const handleCreateTransaction = () => {
+    handleChangeScreenStateToLoading();
+
+    createTransaction(database, {
+      name: transactionName,
+      amount,
+      installment: null,
+      installment_qtd: null,
+      type: transactionType!,
+      date: selectedDate.toISOString(),
+    })
+      .then(() => {
+        handleChangeScreenStateToSuccess();
+      })
+      .catch((error) => {
+        console.log(error);
+        handleChangeScreenStateToError();
+      });
+  };
+
   return (
     <Container>
-      <KeyboardAvoidingView style={styles.avoidView}>
-        <View style={styles.modalHeader}>
-          <Button variant="ghost" Icon={X} onPress={router.back} />
-        </View>
-        <View style={styles.content}>
-          <Input label="Nome" />
-          <Input error label="Nome" errorText="Deu erro tio" />
-          <Input editable={false} />
+      {isScreenStateDefault && (
+        <KeyboardAvoidingView
+          style={styles.avoidView}
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+          <View style={styles.modalHeader}>
+            <Typography>Nova Transação</Typography>
+            <Button variant="ghost" Icon={X} onPress={router.back} />
+          </View>
+          <View style={styles.content}>
+            <Input
+              label="Nome"
+              returnKeyType="next"
+              value={transactionName}
+              onChangeText={setTransactionName}
+              onSubmitEditing={() => {
+                currencyValueRef?.current?.focus?.();
+              }}
+            />
+            <Input
+              label="Valor"
+              keyboardType="number-pad"
+              LeftIcon={DollarSign}
+              value={formatCurrency(amount)}
+              onChangeText={(value) => {
+                const cents = parseCurrencyToCents(value);
+                setAmount(cents);
+              }}
+              ref={currencyValueRef}
+              returnKeyType="next"
+            />
+            <TransactionTypeSwitch onSelect={setTransactionType} />
 
-          {/* Data = DatePicker */}
-          {/* Tipo = Switch */}
+            <View>
+              <Typography variant="label">Data</Typography>
+              <DateTimePicker
+                display="inline"
+                mode="date"
+                value={selectedDate}
+                onChange={(_, date) => {
+                  setSelectedDate(date!);
+                }}
+              />
+            </View>
+            <Button
+              variant="primary"
+              title="Criar"
+              disabled={isCreateButtonDisabled}
+              onPress={handleCreateTransaction}
+            />
+          </View>
+        </KeyboardAvoidingView>
+      )}
+
+      {isScreenStateLoading && (
+        <View style={styles.statesContainer}>
+          <Loading />
         </View>
-        <View style={styles.footer}></View>
-      </KeyboardAvoidingView>
+      )}
+      {isScreenStateError && (
+        <View style={styles.statesContainer}>
+          <LottieView
+            autoPlay
+            style={theme.sizes.errorTransation}
+            source={require('../../../assets/animations/error.json')}
+            loop={false}
+          />
+          <Typography variant="section" style={styles.errorStateText}>
+            Ocorreu um erro inesperado, por favor, tente novamente.
+          </Typography>
+          <Button title="Fechar" onPress={router.back} />
+        </View>
+      )}
+
+      {isScreenStateSuccess && (
+        <View style={styles.statesContainer}>
+          <LottieView
+            autoPlay
+            style={theme.sizes.errorTransation}
+            source={require('../../../assets/animations/success.json')}
+          />
+
+          <AnimatedTypography
+            variant="section"
+            style={styles.errorStateText}
+            entering={FadeIn.duration(3800)}>
+            Transação criada com sucesso!
+          </AnimatedTypography>
+          <Animated.View entering={FadeIn.duration(3800)} style={styles.buttonContainer}>
+            <Button
+              title="Criar uma nova"
+              onPress={handleChangeScreenStateToDefault}
+              style={styles.buttonStyle}
+            />
+            <Button
+              title="Fechar"
+              variant="secondary"
+              onPress={() => router.navigate('/')}
+              style={styles.buttonStyle}
+            />
+          </Animated.View>
+        </View>
+      )}
     </Container>
   );
 }
 
 const styles = StyleSheet.create({
   modalHeader: {
-    alignItems: 'flex-end',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   avoidView: {
     flex: 1,
   },
   content: {
-    borderWidth: 1,
     flex: 1,
     gap: theme.spacing.lg,
   },
@@ -43,5 +187,51 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'red',
   },
-  input: {},
+  switchContainer: {
+    borderWidth: 1,
+    flexDirection: 'row',
+    borderRadius: theme.radius.xl,
+    backgroundColor: theme.colors.surface,
+    borderColor: theme.colors.border,
+  },
+  switchButton: {
+    flex: 1,
+    padding: theme.spacing.lg,
+    flexDirection: 'row',
+    gap: theme.spacing.sm,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  enterIcomeButton: {
+    borderRightWidth: 1,
+    borderRightColor: theme.colors.border,
+    borderTopLeftRadius: theme.radius.xl,
+    borderBottomLeftRadius: theme.radius.xl,
+  },
+  outIncomeButton: {
+    borderTopRightRadius: theme.radius.xl,
+    borderBottomRightRadius: theme.radius.xl,
+  },
+  enterIncomeSelected: {
+    backgroundColor: theme.colors.success50,
+  },
+  outIncomeSelected: {
+    backgroundColor: theme.colors.error50,
+  },
+  statesContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: theme.spacing.md,
+  },
+  errorStateText: {
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    gap: theme.spacing.md,
+  },
+  buttonStyle: {
+    flex: 1,
+  },
 });
