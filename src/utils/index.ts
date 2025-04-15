dec9baf77dd24e95fcf21942b6c8ca8bc7d2a837
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-04-15 16:07:36 -0300

    chore: add parseCurrencyToCents utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,19 @@
 export function formatCurrency(valueInCents: number) {
-  const value = valueInCents / 100;
+  const value = (valueInCents / 100).toFixed(2);
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-  }).format(value);
+  }).format(Number(value));
+}
+
+export function parseCurrencyToCents(value: string) {
+  const numeric = value.replace(/\D/g, '');
+
+  const limited = numeric.slice(0, 12);
+
+  const cents = parseInt(limited || '0', 10);
+
+  return cents;
 }
 
 export function formatDate(date: string, options?: Intl.DateTimeFormatOptions) {
