7787c485bafd00c5e5ae59b17c4a65fae594fd51
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-07-10 18:52:45 -0300

    feat: enhance getTransactionById service to select specific transaction fields and categories

--- src/services/transactions/getTransactionById.ts
+++ src/services/transactions/getTransactionById.ts
@@ -3,7 +3,24 @@ import { supabase } from '../supabase';
 export const getTransactionById = async (user_id: string, transaction_id: number) => {
   return supabase
     .from('transactions')
-    .select()
+    .select(
+      `
+       id,
+        user_id,
+        created_at,
+        name,
+        amount,
+        type,
+        installment,
+        installment_qtd,
+        date,
+        updated_at,
+        categories (
+          id,
+          name
+        )
+      `,
+    )
     .eq('user_id', user_id)
     .eq('id', transaction_id)
     .single()
