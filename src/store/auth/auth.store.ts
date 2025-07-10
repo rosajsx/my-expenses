0af01e5733de74faed52fb306d57b08747f71425
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-07-10 18:49:52 -0300

    refactor: remove unused auth store implementation

--- src/store/auth/auth.store.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-import { AuthStore } from './auth.types';
-
-export const useAuthStore = create<AuthStore>((set) => ({
-  session: null,
-  setSession: (session) => set({ session }),
-}));
