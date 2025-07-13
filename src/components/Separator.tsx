f00945360d70bf1a8db5a9ca0186b35d1eb738c3
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-07-13 10:46:18 -0300

    feat: enhance Separator component with customizable width and color props

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,14 +1,21 @@
-import { colors } from '@/styles/colors';
-import { StyleSheet, View } from 'react-native';
+import { colors, IColors } from '@/styles/colors';
+import { StyleSheet, View, ViewProps } from 'react-native';
 
-export const Separator = () => {
-  return <View style={styles.separator} />;
+interface SeparatorProps {
+  width?: number;
+  color?: IColors;
+  styles?: ViewProps['style'];
+}
+
+export const Separator = ({ width = 0.5, color = 'separator', styles }: SeparatorProps) => {
+  const borderColor = colors[color];
+
+  return <View style={[customStyles.separator, { borderWidth: width, borderColor }, styles]} />;
 };
 
-const styles = StyleSheet.create({
+const customStyles = StyleSheet.create({
   separator: {
     width: '100%',
-    borderWidth: 0.5,
-    borderColor: colors.separator,
+    borderRadius: 8,
   },
 });
